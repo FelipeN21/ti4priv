efc8b7a7e55ee0db554ac153bcb7e5a869573c70
Matheus <matheusmrf@gmail.com>
2022-05-23 17:23:52 -0300

    panel and buttons

---
+++ WorldWar3/Assets/Scripts/TurretSelect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretSelect : MonoBehaviour
+{
+    public void SelectTurret(int turretNumber)
+    {
+
+    }
+}
